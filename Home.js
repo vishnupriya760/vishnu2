d196aaf73b3f73c2d9d4bc6b0278904ddad924b0
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-24 15:49:24 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <div>
+        <div div class="tot">
+        <div class="red">
+        <p>Home</p>
+        <p>About</p>
+        <p>Shop</p>
+        <p>Vendors</p>
+        <p>Pages</p>
+        <p>Block</p>
+        <p>Contact</p>
+<div class="blue">
+      <p>Trending products</p>
+            <div class="or">
+               <div class="bo">
+            <p class="p"> Get 30% Discount Now </p>
+            </div>
+           <button class="b1"><strong>Sale</strong> </button>
+            
+            </div>
+        </div>
+        </div>
+       </div>
+       </div>
+        
+       
+
+  )
+}
+
+export default Home
